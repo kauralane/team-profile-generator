818300567bd562f234defe5f995fbc49ee20b76a
Laura Kane <148717413+kauralane@users.noreply.github.com>
2024-02-10 15:05:37 +0000

    added questions for inquirer prompts and started code to save user inputs.

--- index.js
+++ index.js
@@ -13,3 +13,131 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const managerQuestions = [{
+    type: 'input',
+    message: 'Name:',
+    name: 'name'
+},
+{
+    type: 'input',
+    message: 'ID:',
+    name: 'ID'
+},
+{
+    type: 'input',
+    message: 'Email address:',
+    name: 'email'
+},
+{
+    type: 'input',
+    message: 'Office Number:',
+    name: 'office'
+}
+]
+
+const postManagerMenu = [{
+    type: 'list',
+    message: 'What would you like to do now?',
+    name: 'postManagerMenu',
+    choices: ['Add an engineer', 'Add an intern', 'Finish building the team']
+}]
+
+const engineerQuestions = [{
+    type: 'input',
+    message: 'Name:',
+    name: 'name'
+},
+{
+    type: 'input',
+    message: 'ID:',
+    name: 'ID'
+},
+{
+    type: 'input',
+    message: 'Email:',
+    name: 'email'
+},
+{
+    type: 'input',
+    message: 'GitHub username:',
+    name: 'github'
+}
+]
+
+const internQuestions = [{
+    type: 'input',
+    message: 'Name:',
+    name: 'name'
+},
+{
+    type: 'input',
+    message: 'ID:',
+    name: 'ID'
+},
+{
+    type: 'input',
+    message: 'Email:',
+    name: 'email'
+},
+{
+    type: 'input',
+    message: 'School:',
+    name: 'school'
+}
+]
+
+function promptManagerQuestions() {
+    console.log('Hello. Please input the following details for the manager of the team.')
+    inquirer.prompt(managerQuestions)
+    .then((data) => createManager(data))
+    .then(() => promptMenuQuestions());
+}
+
+function createManager(data) {
+let manager = data;
+console.log(manager);
+}
+
+function promptMenuQuestions() {
+    inquirer.prompt(postManagerMenu)
+    .then((data) => menuResponse(data))
+}
+
+function menuResponse(data) {
+    let response = data.postManagerMenu;
+    console.log(`You have chosen to ${response}`)
+    if (response === 'Add an engineer') {
+        promptEngineerQuestions()
+    } else if (response === 'Add an intern') {
+        promptInternQuestions()
+    } else if (response === 'Finish building the team') {
+        // unsure if this is the right function name below?
+        generateTeam();
+    }
+}
+
+function promptEngineerQuestions() {
+    inquirer.prompt(engineerQuestions)
+    .then((data) => createEngineer(data))
+    .then(() => promptMenuQuestions());
+}
+
+// not sure if these create functions will only let you do one... 
+function createEngineer(data) {
+    let engineer = data;
+    console.log(engineer);
+}
+
+function promptInternQuestions() {
+    inquirer.prompt(internQuestions)
+    .then((data) => createIntern(data))
+    .then(() => promptMenuQuestions());
+}
+
+// not sure if these create functions will only let you do one... 
+function createIntern(data) {
+    let intern = data;
+    console.log(intern);
+}
+
+promptManagerQuestions()
